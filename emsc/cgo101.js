1c603f4cbd90a76d2ec531525147ba8167feccbf
cnyaw <wayncc@gmail.com>
2020-07-08 13:25:06 +0800

    Added touch event support.

--- emsc/cgo101.js
+++ emsc/cgo101.js
@@ -40,6 +40,38 @@ c0.onmousemove = function(e) {
   }
 }
 
+function touchHandler(event) {
+  // https://stackoverflow.com/questions/1517924/javascript-mapping-touch-events-to-mouse-events
+  var touches = event.changedTouches,
+      first = touches[0],
+      type = "";
+  switch(event.type)
+  {
+    case "touchstart": type = "mousedown"; break;
+    case "touchmove":  type = "mousemove"; break;
+    case "touchend":   type = "mouseup"; break;
+    default: return;
+  }
+
+  // initMouseEvent(type, canBubble, cancelable, view, clickCount,
+  //                screenX, screenY, clientX, clientY, ctrlKey,
+  //                altKey, shiftKey, metaKey, button, relatedTarget);
+
+  var simulatedEvent = document.createEvent("MouseEvent");
+  simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                                first.screenX, first.screenY,
+                                first.clientX, first.clientY, false,
+                                false, false, false, 0/*left*/, null);
+
+  first.target.dispatchEvent(simulatedEvent);
+  event.preventDefault();
+}
+
+c0.addEventListener("touchstart", touchHandler, true);
+c0.addEventListener("touchmove", touchHandler, true);
+c0.addEventListener("touchend", touchHandler, true);
+c0.addEventListener("touchcancel", touchHandler, true);
+
 function drawGame() {
   c0.width = window.innerWidth;
   c0.height = window.innerHeight;
